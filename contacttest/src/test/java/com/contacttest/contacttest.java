40464614d0ff2cfa36de7dc1adf183b521b7be8c
user <user@Ranjith>
2021-03-15 14:00:01 +0530

    added on script

---
+++ contacttest/src/test/java/com/contacttest/contacttest.java
@@ -0,0 +1,18 @@
+package com.contacttest;
+
+import org.testng.annotations.Test;
+
+public class contacttest {
+	@Test()
+	public void test()
+	{
+		System.out.println("execute");
+	}
+	@Test()
+	public void test1()
+	{
+		System.out.println("hrrr");
+	
+}
+
+}
